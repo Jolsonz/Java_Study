565262e69b24ba5b52a72e0848bfade779b45cc1
Jolson <jolsonz123@gmail.com>
2020-08-08 10:51:35 +0800

    主要项目

--- Java_ShangGuiGu/day01/src/测试/test.java
+++ Java_ShangGuiGu/day01/src/测试/test.java
@@ -10,6 +10,7 @@ public class test {
     public static void main(String[] args) {
         System.out.println();
         int a = 3;
-        if(a)
+        System.out.println("a = " + a);
+
     }
 }
