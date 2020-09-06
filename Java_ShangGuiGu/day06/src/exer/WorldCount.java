30725b703bab67ba6b777b29cf972cfbda816c2b
Jolson <jolsonz123@gmail.com>
2020-09-06 11:28:29 +0800

    学习到IO流

--- Java_ShangGuiGu/day06/src/exer/WorldCount.java
+++ Java_ShangGuiGu/day06/src/exer/WorldCount.java
@@ -1,12 +1,19 @@
 package exer;
 
 
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
 /**
  * 练习3:获取文本上字符出现的次数,把数据写入文件
  *
  * 思路：
  * 1.遍历文本每一个字符
  * 2.字符出现的次数存在Map中
+ *
+ * 注意写在main函数里的路径默认在当前工程下，不在当前Module里
  *
  * Map<Character,Integer> map = new HashMap<Character,Integer>();
  * map.put('a',18);
@@ -18,7 +25,73 @@ package exer;
  */
 public class WorldCount {
     public static void main(String[] args) {
+        FileReader fr = null;
+        BufferedWriter bw = null;
+        try {
+            ////1.创建Map集合
+            Map<Character, Integer> map = new HashMap<>();
+
+            //2.遍历每一个字符,每一个字符出现的次数放到map中
+            fr = new FileReader("day06\\dbcp.txt");//注意这是在main函数里"dbcp.txt"
+            int c = 0;
+            while ((c = fr.read()) != -1) {
+                char ch = (char) c;
+                if (map.get(ch) == null) {//或者用contains
+                    map.put(ch, 1);
+                } else {
+                    map.put(ch, map.get(ch) + 1);
+                }
+            }
+
+            //3.把map中数据存在文件count.txt
+            //3.1 创建Writer
+            bw = new BufferedWriter(new FileWriter("day06\\WordCount.txt"));
+
+            //3.2 遍历map,再写入数据
+            Set<Map.Entry<Character, Integer>> entrySet = map.entrySet();
+            for (Map.Entry<Character, Integer> entry : entrySet) {
+                switch (entry.getKey()) {
+                    case ' ':
+                        bw.write("空格=" + entry.getValue());
+                        break;
+                    case '\t'://\t表示tab 键字符
+                        bw.write("tab键=" + entry.getValue());
+                        break;
+                    case '\r'://
+                        bw.write("回车=" + entry.getValue());
+                        break;
+                    case '\n'://
+                        bw.write("换行=" + entry.getValue());
+                        break;
+                    default:
+                        bw.write(entry.getKey() + "=" + entry.getValue());
+                        break;
+                }
+                bw.newLine();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            //4.关流
+            if (fr != null) {
+                try {
+                    fr.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+            if (bw != null) {
+                try {
+                    bw.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }
 
     }
-
 }
